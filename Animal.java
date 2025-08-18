455b1da848be9be1a771b9a56edcd623db9557e0
Chitchanok1103 <3661051541103@rmutr.ac.th>
2025-08-18 16:50:57 +0700

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,11 @@
+public class Animal {
+    public void move(){
+        System.out.println("Move...");
+    }
+    public void eat(){
+        System.out.println("eat...");
+    }
+    public void sleep(){
+        System.out.println("sleep...");
+    }
+}
---
+++ Tiger.java
@@ -0,0 +1,9 @@
+public class Tiger extends Animal{
+    public void move(){
+        System.out.println("Tiger runs ");
+    }
+    public void sleep(){
+        System.out.println("sleep Tiger");
+    }
+}
+
